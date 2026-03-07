9e5a9755bc49ebc814440d1932b84af4a25d6316
LucNath <lucasnathan.marc@gmail.com>
2026-03-07 09:59:30 -0300

    DAO - Fornecedor

---
+++ app/src/main/java/com/example/aula03databasepoojunit/dao/FornecedorDAO.java
@@ -0,0 +1,61 @@
+package com.example.aula03databasepoojunit.dao;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import com.example.aula03databasepoojunit.model.Fornecedor;
+import com.example.aula03databasepoojunit.database.DatabaseHelper;
+import java.util.List;
+
+public class FornecedorDAO {
+    private SQLiteDatabase db;
+    private DatabaseHelper dbHelper;
+
+    //Métodos CRUD
+    private void open() {
+    }
+    private void close() {
+    }
+    //Construtor Padrão
+    public FornecedorDAO() {
+    }
+
+    //CREATE - INSERIR UM NOVO REGISTRO (Fornecedor)
+    public long insert(Fornecedor fornecedor) {
+        return 0;
+    }
+
+    //READ - CONSULTAR UM REGISTRO PELO ID (Fornecedor)
+    public List<Fornecedor> select(long id) {
+        return null;
+    }
+
+    //UPDATE - ATUALIZAR UM REGISTRO EXISTENTE (Fornecedor)
+    public int update(Fornecedor fornecedor) {
+        return 0;
+    }
+
+    //DELETE - EXCLUIR UM REGISTRO PELO ID (Fornecedor)
+    public int delete(long id) {
+        return 0;
+    }
+
+    //DELETAR TODOS OS REGISTROS (Fornecedor)
+    public int deleteAll() {
+        return 0;
+    }
+
+    //CONSULTAR TODOS OS REGISTROS (Fornecedor)
+    public int getCount() {
+        return 0;
+    }
+
+    public Fornecedor getFornecedor(long id) {
+        return null;
+    }
+
+    private Fornecedor cursorToFornecedor(Cursor cursor) {
+        return null;
+    }
+
+
+}
